6175708ae2b7379d36862f9d1fe5bb4272ccb84f
leonardo federico <leonardo@plainflow.com>
2017-01-17 22:12:38 +0100

    a few fixes + one Rmd file

--- send_email.R
+++ send_email.R
@@ -1,13 +1,13 @@
 # compile using rmarkdown
 
-install.packages("rmarkdown")
+# install.packages("rmarkdown")
 library(rmarkdown)
 rmarkdown::render("report-ga.Rmd")
 
 install.packages("mailR")
 library(mailR)
 
-?markdown::markdownHTMLOptions
+# ?markdown::markdownHTMLOptions
 
 send.mail(from = "me@leonardofederico.com",
           to = "me@leonardofederico.com",
@@ -19,19 +19,5 @@ send.mail(from = "me@leonardofederico.com",
           authenticate = T,
           send = T)
 
-#compile using knitr
-library(knitr)
-rmarkdown::render("report-ga.Rmd")
 
-send.mail(from = "me@leonardofederico.com",
-          to = "me@leonardofederico.com",
-          subject = "R Markdown Report - knitr",
-          html = T,
-          inline = T,
-          body = "report-ga.html",
-          smtp = list(host.name = "smtp.gmail.com", port = 587, user.name = "me@leonardofederico.com", passwd = "fidelio10", ssl = T),
-          authenticate = T,
-          send = T)
-
-
-# https://www.google.com/settings/security/lesssecureapps
+### https://www.google.com/settings/security/lesssecureapps
